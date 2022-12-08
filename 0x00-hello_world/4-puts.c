c8fc09b167da270734121c2d5a473b9a1ce173fb
kelvin-thegreat <oumaklvn2018@gmail.com>
2022-12-08 05:50:27 -0800

    Task 4. hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
- * main -Entry point
- *
- * main: prints out a string to stdout.
- *
- * return: 0 if success.
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
+
