f6d03d23a5c8b99623dcddb9fbe8fcb9bc0cb78a
kelvin-thegreat <oumaklvn2018@gmail.com>
2022-12-09 11:16:10 -0800

    Task 4.  program that Print all the letters except q and e  in lowercase, followed by a new line

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,11 +4,6 @@
  * main - program that prints the alphabet in lowercase, followed by a new line
  * use the putchar
  * Return: 0
- * Print all the letters except q and e
- * use the putchar function (every other function (printf, puts, etc…) is forbidden)
- * All your code should be in the main function
- * 
- * You can only use putchar twice in your code
  */
 
 int main(void)
