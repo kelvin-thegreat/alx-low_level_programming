ff7c916bafd6ac3e67f8f8d2bf42e3aa9e2d08ec
kelvin-thegreat <oumaklvn2018@gmail.com>
2022-12-13 15:29:05 -0800

    Task 0.program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * main - program that prints Holberton.
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+        int n = 0;
+        char name[13] = "kelvithegreat";
+
+        while (n < 9)
+        {
+                _putchar(str_code[n]);
+                n += 1;
+        }
+        _putchar('\n');
+        return (0);
+}
+
