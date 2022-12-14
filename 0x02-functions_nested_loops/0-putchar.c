c870491ef50462f2b934758ac0a3b99a97d8d607
kelvin-thegreat <oumaklvn2018@gmail.com>
2022-12-14 09:45:27 -0800

    Task 0. program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
 #include "main.h"
+
 /**
- * main - Entry point
+ * main - program that prints Holberton.
  *
- * Return: Always 0 (Success)
+ * Return: int
  */
+
 int main(void)
 {
-	char *sh = "putcharisnoteasy";
+        int n = 0;
+        char str_greet[8] = "_putchar";
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
-
-	return (0);
+        while (n < 8)
+        {
+                _putchar(str_greet[n]);
+                n += 1;
+        }
+        _putchar('\n');
+        return (0);
 }
+
