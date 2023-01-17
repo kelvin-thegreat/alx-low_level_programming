774975d30ab22d268f0d5790125c32f3b6db7af0
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-01-17 07:01:35 -0800

    Task 0. new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog - Struct dog 
+ * @name: name of the dog
+ * @age: The dog's age
+ * @owner: owner of the dog
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
-
