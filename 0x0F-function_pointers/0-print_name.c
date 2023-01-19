0e62de720d63797f30bcf99309808c6ab1293c33
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-01-19 03:07:37 -0800

    Task 0. function that prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
   * print_name - Prototype function name to print name
@@ -10,5 +11,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if ( name != NULL)
+	{
+		f(name);
+	}
 }
