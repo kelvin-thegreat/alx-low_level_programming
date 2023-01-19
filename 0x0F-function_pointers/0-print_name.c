bbc0a07f0d46a00e199e48d586538c036d5de594
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-01-19 03:22:09 -0800

    Task 0. Function to print the name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stddef.h>
 /**
   * print_name - Prints name
   * @name: The name printed
@@ -10,5 +10,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
+	{
 		f(name);
+	}
 }
