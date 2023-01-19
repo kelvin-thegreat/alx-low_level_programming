54d68239f3d60957f5ae55ae2e20ecc842b6a815
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-01-19 04:51:09 -0800

    Task 2. unction that searches for an integer.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - searches for integer
+  * @size: number of elements in array
+  * @cmp: pointer to the function 
+  * @array: array
+  *
+  * Return: ...
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int iterator = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (iterator < size)
+			{
+				if (cmp(array[iterator]))
+					return (iterator);
+
+				iterator++;
+			}
+		}
+	}
+
+	return (-1);
+}
+
