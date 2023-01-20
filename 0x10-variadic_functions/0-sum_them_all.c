4cbf283077a599edcd7843acaa1ec70e5397e50e
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-01-20 00:20:35 -0800

    Task 0. function that returns the sum of all its parameters.

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+  * sum_them_all - ...
+  * @n: ...
+  *
+  * Return: ...
+  */
+
+int sum_them_all(const unsigned int n, ...) 
+{
+	va_list args;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+       	{
+		return 0;
+	}
+	
+	va_start(args, n);
+
+	for (i = 0; i < n; i++) 
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Variadic or varargs Functions
