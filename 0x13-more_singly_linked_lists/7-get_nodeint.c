abab99b4d578d7a75ae6f77cd358f74b0628ba6a
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-02-02 02:07:55 -0800

    Task 7. Get node at Index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * get_nodeint_at_index - ...
+  * @head: ...
+  * @index: ...
+  *
+  * Return: ...
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int current = 0;
+
+	if (head)
+	{
+		while (head)
+		{
+			if (current == index)
+				return (head);
+
+			head = head->next;
+			current++;
+		}
+	}
+
+	return (NULL);
+}i
