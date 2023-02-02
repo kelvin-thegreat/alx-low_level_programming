4576b79939edfb0ebe8fde69cc782d3c879c4196
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-02-01 23:26:24 -0800

    Task 0. Print list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,21 +5,18 @@
 /**
  * listint_len - Returns the number of elements in a linked `listint_t` list.
  * @h: Pointer to the head of the list.
- *
+ * listint_t: list.
  * Return: Number of elements in the list.
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0; /* Keep track of number of elements */
-	/* Iterate through each node in the list */
+	size_t count = 0;
+	
     	while (h != NULL)
     	{
-        	/* Move to the next node in the list */
         	h = h->next;
-		/* Increment the element count */
         	count++;
     	}
 
-    	/* Return the number of elements in the list */
     	return (count);
 }
