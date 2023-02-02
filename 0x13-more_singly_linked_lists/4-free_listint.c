58ebe51fda5aa72bb66aeb6375d6558a9ec00f32
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-02-02 00:34:15 -0800

    Task. 4. Free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,12 +9,13 @@
   */
 void free_listint(listint_t *head)
 {
-	listint_t *temp = head;
+	listint_t *tmp;
 
 	while (head)
 	{
+		tmp = head;
 		head = head->next;
-		free(temp);
+		free(tmp);
 	}
 
 	free(head);
