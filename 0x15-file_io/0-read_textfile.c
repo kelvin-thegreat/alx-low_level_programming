0a44fb2a3143ab23f4341fe0adde6360da706b77
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-02-08 13:10:43 -0800

    Task 0. function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,41 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
 /**
- * read_textfile - function that reads a text file and prints it to the POSIX
- * standard output
- * @filename: file to read
- * @letters: number of letters it should read and print
- * Return: actual numbers it could read and print
+ * read_textfile - that reads a text file and prints
+ * @filename: variable pointer
+ * @letters: size letters
+ * Description: Write a function that reads a text file and prints it
+ * to the POSIX standard output.
+ * Return: the actual number of letters it could read and print, 0 otherwise
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-    	ssize_t read_chars, written_chars;
-    	char *buf;
+	ssize_t file, let, wr;
+	char *text;
 
-    	if (!filename)
-        	return 0;
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
 
-    	fd = open(filename, O_RDONLY);
-    	if (fd == -1)
-        	return 0;
+	if (filename == NULL)
+		return (0);
 
-    	buf = malloc(sizeof(char) * letters);
-    	if (!buf)
-        	return 0;
+	file = open(filename, O_RDONLY);
 
-    	read_chars = read(fd, buf, letters);
-    	if (read_chars == -1) 
+	if (file == -1)
 	{
-        	free(buf);
-        	return 0;
-    	}
+		free(text);
+		return (0);
+	}
 
-    	written_chars = write(STDOUT_FILENO, buf, read_chars);
-    	free(buf);
-    	if (written_chars == -1 || written_chars != read_chars)
-        	return 0;
+	let = read(file, text, letters);
 
-    	return read_chars;
+	wr = write(STDOUT_FILENO, text, let);
+
+	close(file);
+
+	return (wr);
 }
